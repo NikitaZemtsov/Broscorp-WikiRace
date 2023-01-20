54a90d7f3ff24bd4faad848d7ab14cb97b6c1e7f
Nikita Zemtsov <zemtsovnikig@gmail.com>
2023-01-20 12:36:06 +0200

    add description

--- wikiracing.py
+++ wikiracing.py
@@ -9,6 +9,10 @@ from main import logger, session, engine
 from model import PageModel, LinkModel
 import warnings
 
+# The script is always executed for the set time "max_time_script_execution".
+# Even if we make two identical requests one by one.
+# It won't give you an answer right away.
+
 
 requests_per_minute = 100
 links_per_page = 200
